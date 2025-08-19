4b7fde433ea16f849066cccc0f10009c2a27d422
Dalton McMullen <dltn761@gmail.com>
2025-08-18 22:04:30 -0700

    Created button and scores
    Created buttons on the screen to make choice. Also added the displayed
    score. Need to create final logic for game to ensure counter goes up.

--- script.js
+++ script.js
@@ -13,54 +13,69 @@ function getComputerChoice() {
   return compchoice;
 }
 
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+
+document.body.appendChild(rock);
+document.body.appendChild(paper);
+document.body.appendChild(scissors);
+
+
 function getHumanChoice() {
-  let choice = prompt("Rock, Paper, or Scissors?");
+  rock.addEventListener("click", () => choice = "rock");
+  paper.addEventListener("click", () => choice = "paper");
+  scissors.addEventListener("click", () => choice = "scissors");
   return choice;
 }
 
 function playGame() {
 
-
   let humanScore = 0;
   let computerScore = 0;
+  const score = document.createElement("p");
+  document.body.appendChild(score);
+  score.append("Human: ", humanScore, " Computer: ", computerScore);
+
+  const result = document.createElement("div");
+  document.body.appendChild(result);
 
   function playRound(humanChoice, computerChoice) {
-    if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
-      humanScore += 1;
-      console.log("You win! Rock beats Scissors!");
-    } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "paper") {
+    if (humanChoice === computerChoice) {
+      result.textContent = "DRAW!";
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
       computerScore += 1;
-      console.log("You lose! Paper beats Rock!");
-    } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "rock") {
-      console.log("Draw! Rock on Rock!");
-    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "paper") {
-      console.log("Draw! Paper on Paper!");
-    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+      result.textContent = "You lose! Paper beats Rock!";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
       humanScore += 1;
-      console.log("You win! Paper beats Rock!");
-    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
-      computerScore += 1;
-      console.log("You lose! Scissors beats Rock!");
-    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "scissors") {
-      console.log("Draw! Scissor on Scissor!");
-    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+      result.textContent = "You win! Paper beats Rock!";
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
       humanScore += 1;
-      console.log("You win! Scissors beat Paper!");
-    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
-      console.log("You lose! Rock beats Scissor");
+      result.textContent = "You win! Scissors beat Paper!";
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
       computerScore += 1;
+      result.textContent = "You lose! Rock beats Scissor";
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      computerScore += 1;
+      result.textContent = "You lose! Scissors beats Rock!";
     }
   }
 
-  let round = 1;
-  while (round <= 5) {
-    console.log("Round:", round);
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    round += 1;
-    console.log("Human Score:", humanScore, "Computer Score:", computerScore);
-  }
+
+
+  //   let round = 1;
+  //   while (round <= 5) {
+  //     console.log("Round:", round);
+  //     let humanSelection = getHumanChoice();
+  //     let computerSelection = getComputerChoice();
+  //     playRound(humanSelection, computerSelection);
+  //     round += 1;
+  //     console.log("Human Score:", humanScore, "Computer Score:", computerScore);
+  //   }
 }
 
 playGame();
